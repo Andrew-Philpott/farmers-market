a9be382b60664bd9da83a209beb263a94d5c6dfb
Andrew Philpott <andrewphilpott92@gmail.com>
2020-04-27 10:01:39 -0700

    Add test to check if producelistitem can be added to master list.

--- src/__tests__/reducers/produce-list-reducer.test.js
+++ src/__tests__/reducers/produce-list-reducer.test.js
@@ -1,7 +1,42 @@
 import produceListReducer from "../../reducers/produce-list-reducer";
 
 describe("produceListReducer", () => {
+  let action;
+  const availableProduce = {
+    id: 1,
+    month: "January",
+    selection: [
+      "Apples",
+      "Hazelnuts",
+      "Pears",
+      "Garlic",
+      "Mushrooms",
+      "Onions",
+      "Potatoes",
+      "Turnips",
+    ],
+  };
+
   test("Should return default state if there is no action type passed into the reducer", () => {
-    expect(produceListReducer({}, { type: null })).toEqual({});
+    expect(produceListReducer({}, { type: action })).toEqual({});
+  });
+
+  test("Should successfully add a new produce list item to the master produce list", () => {
+    // const items = availableProduce.map((entry) => entry);
+
+    const { id, month, selection } = availableProduce;
+    action = {
+      type: "ADD_PRODUCELISTITEM",
+      id: id,
+      month: month,
+      selection: selection,
+    };
+    expect(produceListReducer({}, { type: action })).toEqual({
+      [id]: {
+        id: id,
+        month: month,
+        selection: selection,
+      },
+    });
   });
 });
